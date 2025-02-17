f4f637674f2fe12ff20baf6a6670935c265fbd6e
jobalfredo <jobalfredo@gmail.com>
2025-02-17 17:28:54 +0800

    added files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loginsignup from './Components/LoginSignup/Loginsignup';
+
+function App() {
+  return (
+    <div>
+      <Loginsignup />
+    </div>
+  );
+}
+
+export default App;
